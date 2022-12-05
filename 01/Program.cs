892f8cb44944274941ac5871dee6718fe034733e
Denifia <mr.l.wale@gmail.com>
2022-12-05 15:44:48 +0800

    day 1 completed

---
+++ 01/Program.cs
@@ -0,0 +1,23 @@
+﻿var elfs = new List<int>();
+var lines = await File.ReadAllLinesAsync("input.txt");
+var currentElf = new List<int>();
+var newElfTime = true;
+for (int i = 0; i < lines.Length; i++)
+{
+    if (string.IsNullOrEmpty(lines[i]))
+    {
+        newElfTime = true;
+        continue;
+    }
+
+    if (newElfTime && currentElf.Any())
+    {
+        elfs.Add(currentElf.Sum());
+        currentElf.Clear();
+    }
+
+    currentElf.Add(int.Parse(lines[i]));
+    newElfTime = false;
+}
+Console.WriteLine($"Part 1 Answer: {elfs.OrderByDescending(x => x).First()}");
+Console.WriteLine($"Part 2 Answer: {elfs.OrderByDescending(x => x).Take(3).Sum()}");
