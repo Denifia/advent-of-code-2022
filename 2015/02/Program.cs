06327394a736941d6d704bed7eb96b8f89c62728
Denifia <mr.l.wale@gmail.com>
2022-12-06 21:27:54 +0800

    2015 day 2 completed

---
+++ 2015/02/Program.cs
@@ -0,0 +1,28 @@
+﻿// Day 2: I Was Told There Would Be No Math
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var presents = lines.Select(x =>
+    {
+        var sides = x.Split('x').Select(y => int.Parse(y)).ToArray();
+        var faces = new int[]
+        {
+            sides[0] * sides[1],
+            sides[1] * sides[2],
+            sides[2] * sides[0]
+        };
+        var area = sides.Aggregate(1, (a, b) => a * b);
+        var perimeters = new int[]
+        {
+            sides[0] + sides[1],
+            sides[1] + sides[2],
+            sides[2] + sides[0]
+        };
+        return ((2 * faces[0]) + (2 * faces[1]) + (2 * faces[2]) + faces.Min(), (perimeters.Order().First() * 2) + area);
+    });
+
+// How many total square feet of wrapping paper should they order?
+Console.WriteLine($"Part 1 Answer: {presents.Sum(x => x.Item1)}");
+
+// How many total feet of ribbon should they order?
+Console.WriteLine($"Part 2 Answer: {presents.Sum(x => x.Item2)}");
