9808ac61016453e1c1fff1a200880a6539edb8ec
Denifia <mr.l.wale@gmail.com>
2022-12-06 20:53:50 +0800

    2015 day 1 completed

---
+++ 2015/01/Program.cs
@@ -0,0 +1,33 @@
+﻿// Day 1: Not Quite Lisp
+
+var instructions = await File.ReadAllTextAsync("input.txt");
+
+var finalLevel = instructions.Select(ProcessInstruction).Sum();
+
+// To what floor do the instructions take Santa?
+Console.WriteLine($"Part 1 Answer: {finalLevel}");
+
+var currentLevel = 0;
+var position = 0;
+for (int i = 0; i < instructions.Length; i++)
+{
+    currentLevel += ProcessInstruction(instructions[i]);
+    if (currentLevel == -1)
+    {
+        position = i + 1;
+        break;
+    }
+}
+
+// What is the position of the character that causes Santa to first enter the basement?
+Console.WriteLine($"Part 2 Answer: {position}");
+
+static int ProcessInstruction(char x)
+{
+    return x switch
+    {
+        '(' => 1,
+        ')' => -1,
+        _ => 0
+    };
+}
