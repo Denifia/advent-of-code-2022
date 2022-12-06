17160f9e4ffb9769ce123800c121c3b2256bb026
Denifia <mr.l.wale@gmail.com>
2022-12-06 08:47:28 +0800

    day 4 completed

---
+++ 04/Program.cs
@@ -0,0 +1,45 @@
+﻿var lines = await File.ReadAllLinesAsync("input.txt");
+
+var containedPairs = 0;
+foreach (var line in lines)
+{
+    // Fully Overlap
+    // line example "1-4,2-3"
+    var assignments = line.Split(',').Select(x =>
+    {
+        var assignment = x.Split('-');
+        return (int.Parse(assignment[0]), int.Parse(assignment[1]));
+    }).ToArray();
+
+    if (assignments[0].Item1 <= assignments[1].Item1 && assignments[0].Item2 >= assignments[1].Item2)
+    {
+        containedPairs++;
+    }
+    else if (assignments[1].Item1 <= assignments[0].Item1 && assignments[1].Item2 >= assignments[0].Item2)
+    {
+        containedPairs++;
+    }
+}
+Console.WriteLine($"Part 1 Answer: {containedPairs}");
+
+containedPairs = 0;
+foreach (var line in lines)
+{
+    // Partial Overlap
+    // line example "1-2,2-3"
+    var assignments = line.Split(',').Select(x =>
+    {
+        var assignment = x.Split('-');
+        return (int.Parse(assignment[0]), int.Parse(assignment[1]));
+    }).ToArray();
+
+    if (assignments[0].Item1 <= assignments[1].Item1 && assignments[0].Item2 >= assignments[1].Item1)
+    {
+        containedPairs++;
+    }
+    else if (assignments[1].Item1 <= assignments[0].Item1 && assignments[1].Item2 >= assignments[0].Item1)
+    {
+        containedPairs++;
+    }
+}
+Console.WriteLine($"Part 2 Answer: {containedPairs}");
