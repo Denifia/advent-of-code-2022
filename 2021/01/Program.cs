4968ccc1805aaa98ca4da497a114eaf2cdb98675
Denifia <mr.l.wale@gmail.com>
2022-12-08 20:56:58 +0800

    2021 day 1 completed

---
+++ 2021/01/Program.cs
@@ -0,0 +1,20 @@
+﻿// Day 1: Sonar Sweep
+
+var measurements = File.ReadAllLines("input.txt").Select(int.Parse).ToArray();
+var increases = 0;
+for (int i = 1; i < measurements.Length; i++)
+{
+    increases += measurements[i] > measurements[i - 1] ? 1 : 0;
+}
+
+// How many measurements are larger than the previous measurement?
+Console.WriteLine($"Part 1 Answer: {increases}");
+
+increases = 0;
+for (int i = 1; i < measurements.Length - 2; i++)
+{
+    increases += measurements[i..(i+3)].Sum() > measurements[(i-1)..(i+2)].Sum() ? 1 : 0;
+}
+
+// Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+Console.WriteLine($"Part 2 Answer: {increases}");
