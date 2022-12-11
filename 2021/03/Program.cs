66ab4cb3864c7102cd63a0d315a15c63a613f6e2
Denifia <mr.l.wale@gmail.com>
2022-12-11 20:43:05 +0800

    2021 day 3 part 1

---
+++ 2021/03/Program.cs
@@ -0,0 +1,31 @@
+﻿// Day 3: Binary Diagnostic
+
+var rows = File.ReadAllLines("input.txt");
+var columns = new List<string>();
+foreach (var row in rows)
+{
+    for (int i = 0; i < row.Length; i++)
+    {
+        if (columns.Count < i + 1) 
+            columns.Add(string.Empty);
+
+        columns[i] += row[i];
+    }
+}
+
+var gammaRateBits = columns.Select(col => col.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key).ToArray();
+var epsilonRateBits = gammaRateBits.Select(x => x switch
+{
+    '0' => '1',
+    '1' => '0',
+    _ => throw new NotSupportedException()
+}).ToArray();
+
+var gammaRate = Convert.ToInt32(new string(gammaRateBits), 2);
+var epsilonRate = Convert.ToInt32(new string(epsilonRateBits), 2);
+
+// What is the power consumption of the submarine?
+Console.WriteLine($"Part 1 Answer: {gammaRate * epsilonRate}");
+
+// question 2
+//Console.WriteLine($"Part 2 Answer: {Convert.ToInt32(epsilonRate.ToString(), 2)}");
