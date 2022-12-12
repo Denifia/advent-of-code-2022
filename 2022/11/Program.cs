402c9902c4d1967b17463b09d0fcbb142eb5fd79
Denifia <mr.l.wale@gmail.com>
2022-12-12 21:35:52 +0800

    2022 day 11 working

--- 2022/11/Program.cs
+++ 2022/11/Program.cs
@@ -1,77 +1,110 @@
 ﻿// Day 11: Monkey in the Middle
 
-var lines = File.ReadAllLines("test-input.txt");
+using System.Numerics;
 
-var monkeys = new List<Monkey>();
-var monkeyBuilder = new MonkeyBuilder();
-foreach (var line in lines)
+public static int reliefValue = 0;
+
+var monkeyBusiness = Simulate(20, withRelief: true);
+// What is the level of monkey business after 20 rounds of stuff-slinging simian shenanigans?
+Console.WriteLine($"Part 1 Answer: {monkeyBusiness}");
+
+monkeyBusiness = Simulate(10000, withRelief: false);
+// Starting again from the initial state in your puzzle input, what is the level of monkey business after 10000 rounds?
+Console.WriteLine($"Part 2 Answer: {monkeyBusiness}");
+
+
+ulong Simulate(int rounds, bool withRelief)
 {
-    if (string.IsNullOrWhiteSpace(line))
+    var lines = File.ReadAllLines("input.txt");
+
+    var monkeys = new List<Monkey>();
+    var monkeyBuilder = new MonkeyBuilder();
+    foreach (var line in lines)
     {
-        monkeys.Add(monkeyBuilder.Build());
-        monkeyBuilder = new MonkeyBuilder();
-        continue;
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            monkeys.Add(monkeyBuilder.Build());
+            monkeyBuilder = new MonkeyBuilder();
+            continue;
+        }
+
+        monkeyBuilder.ProcessLine(line);
     }
 
-    monkeyBuilder.ProcessLine(line);
-}
+    // last monkey has not trailing newline
+    monkeys.Add(monkeyBuilder.Build());
 
-// last monkey has not trailing newline
-monkeys.Add(monkeyBuilder.Build());
+    reliefValue = monkeys.Select(x => x.TestNumber).Aggregate(1, (x, y) => x * y);
 
-var monkeyBusinessAtRound20 = 0;
-for (int round = 1; round <= 20; round++)
-{
-    foreach (var monkey in monkeys)
+    ulong[] inspectedItems = Enumerable.Range(0, monkeys.Count).Select(x => (ulong)0).ToArray();
+    for (int round = 1; round <= rounds; round++)
     {
-        var monkeyNumber = monkeys.IndexOf(monkey);
-        while (monkey.HoldingItems)
+        foreach (var monkey in monkeys)
         {
-            monkey.InspectNextItem();
-            (var catchingMonkey, var thrownItem) = monkey.ThrowNextItem();
-            monkeys[catchingMonkey].CatchItem(thrownItem);
+            //var monkeyNumber = monkeys.IndexOf(monkey);
+            while (monkey.HoldingItems)
+            {
+                monkey.InspectNextItem(withRelief);
+                (var catchingMonkey, var thrownItem) = monkey.ThrowNextItem();
+                monkeys[catchingMonkey].CatchItem(thrownItem);
+            }
         }
     }
 
-    if (round == 20)
-    {
-        monkeyBusinessAtRound20 = monkeys
-            .OrderByDescending(x => x.InspectedItems)
-            .Take(2)
-            .Aggregate(1, (x, monkey) => x * monkey.InspectedItems);
-    }
+    return monkeys
+        .OrderByDescending(x => x.InspectedItems)
+        .Take(2)
+        .Aggregate<Monkey, ulong>(1, (x, monkey) => x * monkey.InspectedItems);
 }
-
-// What is the level of monkey business after 20 rounds of stuff-slinging simian shenanigans?
-Console.WriteLine($"Part 1 Answer: {monkeyBusinessAtRound20}");
-
-//var monkeyBusinessAtRound10000 = monkeys
-//.OrderByDescending(x => x.InspectedItems)
-//.Take(2)
-//.Aggregate(1, (x, monkey) => x * monkey.InspectedItems);
-
-// Starting again from the initial state in your puzzle input, what is the level of monkey business after 10000 rounds?
-//Console.WriteLine($"Part 2 Answer: {monkeyBusinessAtRound10000}");
-
 class Item
 {
-    public long WorryLevel { get; set; }
-    public Item(int worryLevel) => WorryLevel = worryLevel;
-    public void WorryRelief() => WorryLevel /= 3;
+    private BigInteger _worryLevel;
+
+    public Item(int worryLevel)
+    {
+        _worryLevel = new BigInteger(worryLevel);
+    }
+
+    public void IncreaseWorryLevelBy(int amount)
+    {
+        _worryLevel += amount;
+    }
+
+    public void MultiplyWorryLevelBy(int amount)
+    {
+        _worryLevel *= amount;
+    }
+
+    public void MultiplyWorryLevelByWorryLevel()
+    {
+        _worryLevel *= _worryLevel;
+    }
+
+    public void WorryRelief()
+    {
+        _worryLevel /= 3;
+    }
+
+    public void WorryRelief2()
+    {
+        _worryLevel %= reliefValue;
+    }
+
+    public bool Test(int amount) => _worryLevel % amount == 0;
 }
 
 class Monkey
 {
-    public int InspectedItems { get ; private set; }
+    public ulong InspectedItems { get; private set; }
     public bool HoldingItems => _items.Any();
 
     private Queue<Item> _items = new Queue<Item>();
     private readonly Action<Item> _inspectionOperation;
-    private readonly Func<Item, bool> _test;
+    public int TestNumber { get; private set; }
     private readonly int _nextMonkeyIfTestTrue;
     private readonly int _nextMonkeyIfTestFalse;
 
-    public Monkey(int[] startingItems, Action<Item> operation, Func<Item, bool> test, int nextMonkeyIfTestTrue, int nextMonkeyIfTestFalse)
+    public Monkey(int[] startingItems, Action<Item> operation, int testNumber, int nextMonkeyIfTestTrue, int nextMonkeyIfTestFalse)
     {
         foreach (var item in startingItems)
         {
@@ -79,23 +112,31 @@ class Monkey
         }
 
         _inspectionOperation = operation;
-        _test = test;
+        TestNumber = testNumber;
         _nextMonkeyIfTestTrue = nextMonkeyIfTestTrue;
         _nextMonkeyIfTestFalse = nextMonkeyIfTestFalse;
     }
 
-    public void InspectNextItem()
+    public void InspectNextItem(bool withRelief = false)
     {
         var item = _items.Peek();
         _inspectionOperation(item);
-        item.WorryRelief();
+        if (withRelief)
+        {
+            item.WorryRelief();
+        }
+        else
+        {
+            item.WorryRelief2();
+        }
+
         InspectedItems++;
     }
 
     public (int, Item) ThrowNextItem()
     {
         var item = _items.Dequeue();
-        return _test.Invoke(item) switch
+        return item.Test(TestNumber) switch
         {
             true => (_nextMonkeyIfTestTrue, item),
             false => (_nextMonkeyIfTestFalse, item)
@@ -125,9 +166,9 @@ class MonkeyBuilder
             ["Operation:", _, _, _, ..var parts] =>
                 parts switch
                 {
-                    ["+", var amount] => builder => { builder._operation = item => item.WorryLevel += int.Parse(amount); },
-                    ["*", "old"] => builder => { builder._operation = item => item.WorryLevel *= item.WorryLevel; },
-                    ["*", var amount] => builder => { builder._operation = item => item.WorryLevel *= int.Parse(amount); },
+                    ["+", var amount] => builder => { builder._operation = item => item.IncreaseWorryLevelBy(int.Parse(amount)); },
+                    ["*", "old"] => builder => { builder._operation = item => item.MultiplyWorryLevelByWorryLevel(); },
+                    ["*", var amount] => builder => { builder._operation = item => item.MultiplyWorryLevelBy(int.Parse(amount)); },
                     _ => throw new NotSupportedException()
                 },
             ["Test:", _, _, var amount] => builder => { builder._testDivideNumber = int.Parse(amount); },
@@ -138,5 +179,5 @@ class MonkeyBuilder
         action.Invoke(this);
     }
 
-    public Monkey Build() => new Monkey(_items, _operation, item => item.WorryLevel % _testDivideNumber == 0, _nextMonkeyIfTestTrue, _nextMonkeyIfTestFalse);
+    public Monkey Build() => new Monkey(_items, _operation, _testDivideNumber, _nextMonkeyIfTestTrue, _nextMonkeyIfTestFalse);
 }
