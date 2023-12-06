2674cc4eb4b834ab3689028b5891b42599fa1542
Denifia <mr.l.wale@gmail.com>
2023-12-06 20:15:17 +0800

    starting 2023 challenges

---
+++ 2023/01/Program.cs
@@ -0,0 +1,59 @@
+﻿// Day 1: Trebuchet?!
+
+var lines = File.ReadAllLines("input.txt");
+int sum = 0;
+
+//var digits = "0123456789";
+//foreach (var line in lines)
+//{
+//    var first = line.First(digits.Contains);
+//    var last = line.Last(digits.Contains);
+//    sum += int.Parse($"{first}{last}");
+//}
+
+//// question 1
+//Console.WriteLine($"Part 1 Answer: {sum}");
+
+var numbers = new Dictionary<string, string>()
+{
+    { "0", "0" },
+    { "zero", "0" },
+    { "1", "1" },
+    { "one", "1" },
+    { "2", "2" },
+    { "two", "2" },
+    { "3", "3" },
+    { "three", "3" },
+    { "4", "4" },
+    { "four", "4" },
+    { "5", "5" },
+    { "five", "5" },
+    { "6", "6" },
+    { "six", "6" },
+    { "7", "7" },
+    { "seven", "7" },
+    { "8", "8" },
+    { "eight", "8" },
+    { "9", "9" },
+    { "nine", "9" },
+};
+
+foreach (var line in lines)
+{
+    var first = numbers
+        .Select(x => new { index = line.IndexOf(x.Key), x.Value })
+        .Where(x => x.index >= 0)
+        .OrderBy(x => x.index)
+        .First().Value;
+
+    var last = numbers
+        .Select(x => new { index = line.LastIndexOf(x.Key), x.Value })
+        .Where(x => x.index >= 0)
+        .OrderByDescending(x => x.index + x.Value.Length)
+        .First().Value;
+
+    sum += int.Parse($"{first}{last}");
+}
+
+// question 2
+Console.WriteLine($"Part 2 Answer: {sum}");
---
+++ 2023/02/Program.cs
@@ -0,0 +1,40 @@
+﻿// Day 2: Cube Conundrum
+
+var lines = File.ReadAllLines("test-input.txt");
+
+lines.Select(line =>
+{
+    var gameNumber = int.Parse(line.Substring(5, line.IndexOf(":")));
+    var handfulls = line
+        .Substring(line.IndexOf(":") + 2)
+        .Split("; ")
+        .SelectMany(draw => 
+            draw.Split(", ")
+                .Select(x => x.Split(" "))
+                .Select(x => x[1] switch
+                    {
+                        "red" => new Dice(Color.Red, int.Parse(x[0])),
+                        "blue" => new Dice(Color.Blue, int.Parse(x[0])),
+                        "green" => new Dice(Color.Green, int.Parse(x[0])),
+                    })
+        .ToArray();
+    return new Game(gameNumber, handfulls);
+});
+
+// question 1
+Console.WriteLine($"Part 1 Answer: {true}");
+
+// question 2
+Console.WriteLine($"Part 2 Answer: {true}");
+
+
+record Game(int Number, Handfulls[] Handfulls);
+record Handfulls(int Red, int Blue, int Green);
+record Bag(int Red, int Blue, int Green);
+record Dice(Color Color, int Amount);
+enum Color
+{
+    Red = 0,
+    Blue = 1,
+    Green = 2,
+}
