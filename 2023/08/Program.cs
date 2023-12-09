9866ed28efc30668ef8638d27491f36242913032
Denifia <mr.l.wale@gmail.com>
2023-12-09 11:01:03 +0800

    2023-08 done

--- 2023/08/Program.cs
+++ 2023/08/Program.cs
@@ -4,36 +4,60 @@ var lines = File.ReadAllLines("input.txt");
 var (instructions, map) = ParseInput(lines);
 var repeatingInstructions = RepeatingInstructions(instructions);
 
-var starts = map.Where(x => x.Key.EndsWith("Z")).Select(x => x.Key).ToArray();
-var walkers = starts.Select(x => WalkMap(x, repeatingInstructions, map).GetEnumerator()).ToArray();
-
-var count = 0;
-while (true)
-{
-    foreach (var walker in walkers)
-    {
-        walker.MoveNext();
-    }
-    count++;
-    if (walkers.All(x => x.Current.EndsWith("Z")))
-    {
-        break;
-    }
-}
+var starts = map.Where(x => x.Key.EndsWith("A")).Select(x => x.Key).ToArray();
+var counts = starts.Select(x => WalkMap(x, repeatingInstructions, map)).ToArray();
+var result = GetLowestCommonMultiple(counts);
 
 // question 
-Console.WriteLine($"Answer: {count}");
+Console.WriteLine($"Answer: {result}");
 
-IEnumerable<string> WalkMap(string start, IEnumerable<int> repeatingInstructions, Dictionary<string, string[]> map)
+static long GetLowestCommonMultiple(int[] numbers)
 {
+    long result = numbers[0];
+    for (int i = 1; i < numbers.Length; i++)
+    {
+        result = LowestCommonMultiple(result, numbers[i]);
+    }
+    return result;
+}
+
+static long GreatestCommonFactor(long a, long b)
+{
+    while (b != 0)
+    {
+        long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+static long LowestCommonMultiple(long a, long b)
+{
+    var gcf = GreatestCommonFactor(a, b);
+    Console.WriteLine($"Highest common factor of {a} and {b} is {gcf}");
+
+    var lcm = (a / gcf) * b;
+    Console.WriteLine($"Lowest common multiple of {a} and {b} is {lcm}");
+
+    return lcm;
+}
+
+int WalkMap(string start, IEnumerable<int> repeatingInstructions, Dictionary<string, string[]> map)
+{
+    var count = 0;
     var row = start;
     foreach (var instruction in repeatingInstructions)
     {
+        count++;
         row = map[row][instruction];
-        yield return row;
+        if (row.EndsWith("Z"))
+        {
+            break;
+        }
     }
+    return count;
 }
-
 
 (int[], Dictionary<string, string[]>) ParseInput(string[] lines)
 {
