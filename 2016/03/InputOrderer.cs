00cba49918932e2533fbf46289fbe420912ded98
Denifia <mr.l.wale@gmail.com>
2024-11-27 20:48:16 +0800

    2016-03 part 2 done

---
+++ 2016/03/InputOrderer.cs
@@ -0,0 +1,35 @@
+﻿// Title
+
+using System.Collections;
+
+class InputOrderer : IEnumerable<Triangle>
+{
+    private readonly List<int> col1 = [];
+    private readonly List<int> col2 = [];
+    private readonly List<int> col3 = [];
+
+    public void Add((int One, int Two, int Three) inputRow)
+    {
+        col1.Add(inputRow.One);
+        col2.Add(inputRow.Two);
+        col3.Add(inputRow.Three);
+    }
+
+    public IEnumerator<Triangle> GetEnumerator()
+    {
+        var enumerator = col1.Concat(col2).Concat(col3).GetEnumerator();
+        
+        while (enumerator.MoveNext())
+        {
+            Triangle triangle = new();
+            triangle.X = enumerator.Current;
+            enumerator.MoveNext();
+            triangle.Y = enumerator.Current;
+            enumerator.MoveNext();
+            triangle.Z = enumerator.Current;
+            yield return triangle;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
--- 2016/03/Program.cs
+++ 2016/03/Program.cs
@@ -1,4 +1,4 @@
-﻿// Title
+﻿// Day 3: Squares With Three Sides
 
 global using Triangle = (int X, int Y, int Z);
 using aoc;
@@ -18,6 +18,20 @@ foreach (var line in lines)
 // question 1
 Console.WriteLine($"Part 1 Answer: {possibleTriangles}");
 
-// question 2
-Console.WriteLine($"Part 2 Answer: {true}");
+InputOrderer inputOrderer = [];
+possibleTriangles = 0;
+foreach (var line in lines)
+{
+    var inputRow = InputConverter.Convert(line.AsSpan());
+    inputOrderer.Add(inputRow);
+}
+foreach (var triangle in inputOrderer)
+{
+    if (TriangleVerifier.IsPossible(triangle))
+    {
+        possibleTriangles++;
+    }
+}
 
+// question 2
+Console.WriteLine($"Part 2 Answer: {possibleTriangles}");
